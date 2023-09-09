f4bc55b41ebe1e0d213181ceda794868ed66a0aa
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-09 09:11:11 -0300

    ✨ feat: get person search

--- 03-fast-api-crud/main.py
+++ 03-fast-api-crud/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from typing import Optional
 
@@ -19,6 +19,31 @@ with open("people.json", "r") as f:
 
 
 @app.get("/person/{p_id}", status_code=200)
-def get_person(p_id: int):
+def get_person_by_id(p_id: int):
     person = [p for p in people if p["id"] == p_id]
     return person[0] if len(person) > 0 else {}
+
+
+@app.get("/search", status_code=200)
+def get_person(
+    age: Optional[int] = Query(
+        None, title="Age", description="The age to filter for"
+    ),
+    name: Optional[str] = Query(
+        None, title="Name", description="The name to filter for"
+    ),
+):
+    people1 = [p for p in people if p["age"] == age]
+
+    if name is None:
+        if age is None:
+            return people
+        else:
+            return people1
+    else:
+        people2 = [p for p in people if name.lower() in p["name"].lower()]
+        if age is None:
+            return people2
+        else:
+            combined = [p for p in people1 if p in people2]
+            return combined
